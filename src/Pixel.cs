c63d5191e9597070e21e5d49190aa5160461da62
bmarron18 <bmarron@pdx.edu>
2017-05-10 09:16:36 -0700

    First round standardization and QAQC-driven changes

---
+++ src/Pixel.cs
@@ -0,0 +1,21 @@
+using Landis.SpatialModeling;
+using Landis.SpatialModeling.CoreServices;
+
+namespace Landis.Library.InitialCommunities
+{
+    public class Pixel : Landis.SpatialModeling.Pixel
+        //: SingleBandPixel<ushort>
+    {
+        public Pixel()
+            : base()
+        {
+        }
+
+        //---------------------------------------------------------------------
+
+        public Pixel(ushort band0)
+        //    : base(band0)
+        {
+        }
+    }
+}
